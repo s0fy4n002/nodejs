97a8c6de5275c009a9ab3cb9b748b3a7d09bbf66
s0fy4n002 <s0fy4n002@gmail.com>
2021-09-22 14:29:09 +0700

    minimalis

--- index.js
+++ index.js
@@ -1,16 +1,36 @@
-const express = require('express')
-const app  = express()
-const ejs = require('ejs')
-const path = require('path')
+// const express = require('express');
+// const app  = express();
+// const router = express.Router();
+// const ejs = require('ejs');
+// const path = require('path');
 
-app.use(express.json())
-app.use(express.urlencoded({extended: true}))
-app.use(express.static('public'))
-app.set('views', path.join(__dirname, 'views'))
-app.set('view engine', 'ejs')
+// app.use(express.json());
+// app.use(express.urlencoded({extended: true}));
+// app.use(express.static('public'));
+// app.set('views', path.join(__dirname, 'views'));
+// app.set('view engine', 'ejs');
 
-app.get('/', (req, res)=>{
-    res.render("page/index")
+// app.get('/', (req, res)=>{
+//     res.render("page/index")
+// });
+
+// app.listen(3000, ()=> console.log("server up"));
+
+
+var http = require('http');
+var fs = require('fs')
+
+const server = http.createServer((req, res) => {
+    fs.readFile('./views/page/index.ejs', (err, data)=>{
+        if(err){
+            res.writeHead(404);
+            res.write("Error: file tidak ditemukan!");
+        }else{
+            res.writeHead(200, {'Content-Type': 'text/html'});
+            res.write(data);
+        }
+        res.end();
+    })
 })
 
-app.listen(3000, ()=> console.log("server up"))
+server.listen(8080, () => console.log('server running'))
